47c80423d7a51e48293556d2fd1f750e8e3fc53b
labadcloyd <abadcloyd@gmail.com>
2021-10-01 20:39:28 +0800

    lists

---
+++ list.py
@@ -0,0 +1,16 @@
+myFriends = ['joe', 'john', 'doe', 'meyer']
+
+myFriends.append('myself')
+print(myFriends)
+
+myFriends.pop()
+print(myFriends)
+
+num = myFriends.count('doe')
+print(num)
+
+ind = myFriends.index('meyer')
+print(ind)
+
+myFriends.reverse()
+print(myFriends)
